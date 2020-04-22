535b9184bb6edbff2db8dc03348db29e729d5485
David Brooks <dave@bcs.co.nz>
2020-04-22 17:55:23 +1200

    Tweak display for annotation viewer.

--- src/main.js
+++ src/main.js
@@ -6,13 +6,11 @@ window.onload = function() {
 //    mapManager.loadMap('NCBITaxon:9606', 'map1', { annotatable: true });
     mapManager.loadMap('NCBITaxon:10114', 'map1', () => {}, {
 //    mapManager.loadMap('NCBITaxon:9606', 'map1', {
-        //tooltips: true,
+        tooltips: true,
         //debug: true,
-        navigationControl: 'top-right',
+        //navigationControl: 'top-right',
         searchable: true,
-        featureInfo: true,
-        zoom: 6,
-        center: [4.737, -3.422]
+        featureInfo: true
     });
 
     //mapManager.loadMap('demo', 'map2', { annotatable: true, debug: true });
