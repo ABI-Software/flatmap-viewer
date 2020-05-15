bb9b03a5d3120c714998243d35c4a453091da33a
David Brooks <dave@bcs.co.nz>
2020-05-15 12:23:24 +1200

    By default a feature's fill is almost transparent.

--- src/styling.js
+++ src/styling.js
@@ -157,7 +157,7 @@ export class FeatureFillLayer
                     'case',
                     ['boolean', ['feature-state', 'active'], false], 0.7,
                     ['boolean', ['feature-state', 'highlighted'], false], 0.5,
-                    0.7
+                    0.1
                 ]
             }
         };
