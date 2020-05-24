e7e8620f45287452d5381d848696c36650d7d37e
David Brooks <dave@bcs.co.nz>
2020-05-24 21:56:00 +1200

    Add title and aria label to buttons.

--- src/search.js
+++ src/search.js
@@ -41,7 +41,7 @@ export class SearchControl
         this._container.className = 'mapboxgl-ctrl search-control';
         // https://iconmonstr.com/magnifier-6-svg/
         this._container.innerHTML = `<input id="search-control-input" type="search" placeholder="Search..." style="visibility: hidden"/>
-    <button class="control-button" id="search-control-button">
+    <button class="control-button" id="search-control-button" type="button" title="Search flatmap" aria-label="Search flatmap">
      <svg xmlns="http://www.w3.org/2000/svg" id="search-control-icon" viewBox="0 0 24 24">
        <path d="M21.172 24l-7.387-7.387c-1.388.874-3.024 1.387-4.785 1.387-4.971 0-9-4.029-9-9s4.029-9 9-9 9 4.029 9 9c0 1.761-.514 3.398-1.387 4.785l7.387 7.387-2.828 2.828zm-12.172-8c3.859 0 7-3.14 7-7s-3.141-7-7-7-7 3.14-7 7 3.141 7 7 7z"/>
      </svg>
