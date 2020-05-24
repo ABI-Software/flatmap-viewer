85e4ed6e8f7db2d0176f887854f6aac13aa30701
David Brooks <dave@bcs.co.nz>
2020-05-24 21:57:27 +1200

    Make sure search control is on top-right side.

--- src/search.js
+++ src/search.js
@@ -50,6 +50,12 @@ export class SearchControl
         return this._container;
     }
 
+    getDefaultPosition()
+    //==================
+    {
+        return 'top-right';
+    }
+
     onRemove()
     //========
     {
